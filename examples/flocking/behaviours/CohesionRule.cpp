427f5184014ad935b9101e0a89c00fec1dd8ce9f
Dillon D <dillon.drummond@mymail.champlain.edu>
2022-09-03 17:33:43 -0400

    Adding tentative cohesion rule, when cohesion is the only rule, the paths always become circular unlike the example project

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -7,7 +7,17 @@ Vector2 CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid*
     // todo: add your code here to make a force towards the center of mass
     // hint: iterate over the neighborhood
 
+    for (Boid* neighbor : neighborhood)
+    {
+        cohesionForce += neighbor->getPosition();
+    }
 
+    if (neighborhood.size() > 0)
+    {
+        cohesionForce = cohesionForce / neighborhood.size();
+    }
+
+    cohesionForce -= boid->getPosition();
 
     return cohesionForce;
 }
