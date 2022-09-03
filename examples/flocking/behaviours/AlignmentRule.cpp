da02b91870ee43fbe541d5b4f554acc7ca80fbdf
Dillon D <dillon.drummond@mymail.champlain.edu>
2022-09-03 15:38:26 -0400

    Adding alignment rule

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -7,5 +7,13 @@ Vector2 AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
     // todo: add your code here to align each boid in a neighborhood
     // hint: iterate over the neighborhood
 
+    for (Boid* neighbor : neighborhood)
+    {
+        if (Vector2::getDistance(neighbor->getPosition(), boid->getPosition()) < boid->getDetectionRadius())
+        {
+            averageVelocity += neighbor->getVelocity() * getBaseWeightMultiplier();
+        }
+    }
+
     return Vector2::normalized(averageVelocity);
 }
